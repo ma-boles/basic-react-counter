435559d642d5d32408bf16841743663198d7265a
ma-boles <120141149+ma-boles@users.noreply.github.com>
2023-06-12 16:45:09 -0700

    Add files via upload

---
+++ Counter.js
@@ -0,0 +1,29 @@
+import React from "react"
+
+export default function Counter () {
+
+    const [count,setCount] = React.useState(0)
+    
+    function add() {
+        setCount(count + 1) 
+    }
+
+    function minus () {
+        setCount(count - 1)
+    }
+
+    return (
+        <div className="counter">
+      
+        <h1 className="title">Basic Counter</h1>
+        
+        <div className="counterDisplay">
+            <h2>{count}</h2>
+        </div>
+        
+        <button onClick={minus}>-</button>
+        <button onClick={add}>+</button>
+        </div>
+
+    )
+}
